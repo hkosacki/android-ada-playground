e4d9a00ffdf177e6be6ea0ee50633a0dcacedb87
Hubert Kosacki <hkosacki@gmail.com>
2020-04-16 09:41:51 +0200

    Demo accessibility focus app

---
+++ app/src/main/java/com/example/seekbartest/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.seekbartest
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+const val TAG = "SeekBar"
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+
+}
